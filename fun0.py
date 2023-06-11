3305ce2707397f78a6b006b59769d167d6eda1c3
xukq <xu_kangqi@mail.dlut.edu.cn>
2023-06-11 16:22:33 +0800

    Chap.6

--- fun0.py
+++
@@ -1,9 +0,0 @@
-def factional(n):
-    '''return n!'''
-    return 1 if n < 2 else n * factional(n - 1)
-
-
-if __name__ == '__main__':
-    print(factional(42))
-    print(factional.__doc__)
-    print(type(factional))
